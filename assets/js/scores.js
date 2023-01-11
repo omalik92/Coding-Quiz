e758e7d85e16a01d5e5532cc3b545face60b7e7b
Omar <omalik0192@googlemail.com>
2023-01-11 19:55:31 +0000

    full quiz gae working

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,7 @@
 scores = JSON.parse(localStorage.getItem("scores"));
+
 highScores = document.querySelector("#highscores");
+clearScores = document.querySelector("#clear");
 //sort array
 scores.sort((a, b) => {
   return b.score - a.score;
@@ -8,7 +10,15 @@ for (i = 0; i < scores.length; i++) {
   list = document.createElement("li");
   initials = scores[i].initials;
   score = scores[i].score;
-
   list.textContent = `${initials} - ${score}`;
   highScores.appendChild(list);
 }
+clearScores.addEventListener("click", (event) => {
+  var element = event.target;
+  if (element.matches("button")) {
+    for (i = 0; i < scores.length; i++) {
+      highScores.removeChild(highScores.firstChild);
+    }
+    localStorage.clear();
+  }
+});
